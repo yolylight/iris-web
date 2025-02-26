50abb480bca05ee5f886906c2eca27d435e17530
whikernel <whitekernel@whitekernel.fr>
2025-02-26 15:01:06 +0100

    [FIX] CodeQL replacement of s ausbstring with itself

--- ui/src/pages/case.timeline.js
+++ ui/src/pages/case.timeline.js
@@ -1162,18 +1162,18 @@ function timelineToCsv(){
     csv_data = "event_date(UTC),event_title,event_description,event_tz,event_date_wtz,event_category,event_tags,linked_assets,linked_iocs\n";
     for (index in current_timeline) {
         item = current_timeline[index];
-        content = item.event_content.replace(/"/g, '\"');
+        content = item.event_content.replace(/"/g, '\\"');
         content_parsed = content.replace(/(\r?\n)+/g, ' - ');
-        title = item.event_title.replace(/"/g, '\"');
-        tags = item.event_tags.replace(/"/g, '\"');
+        title = item.event_title.replace(/"/g, '\\"');
+        tags = item.event_tags.replace(/"/g, '\\"');
         assets = "";
         for (k in item.assets) {
-            asset = item.assets[k].name.replace(/"/g, '\"');
+            asset = item.assets[k].name.replace(/"/g, '\\"');
             assets += `${asset};`;
         }
         iocs = "";
         for (k in item.iocs) {
-            ioc = item.iocs[k].name.replace(/"/g, '\"');
+            ioc = item.iocs[k].name.replace(/"/g, '\\"');
             iocs += `${ioc};`;
         }
         csv_data += `"${item.event_date}","${title}","${content_parsed}","${item.event_tz}","${item.event_date_wtz}","${item.category_name}","${tags}","${assets}","${iocs}"\n`;
@@ -1186,18 +1186,18 @@ function timelineToCsvWithUI(){
     for (index in current_timeline) {
 
         item = current_timeline[index];
-        content = item.event_content.replace(/"/g, '\"');
+        content = item.event_content.replace(/"/g, '\\"');
         content_parsed = content.replace(/(\r?\n)+/g, ' - ');
-        title = item.event_title.replace(/"/g, '\"');
-        tags = item.event_tags.replace(/"/g, '\"');
+        title = item.event_title.replace(/"/g, '\\"');
+        tags = item.event_tags.replace(/"/g, '\\"');
         assets = "";
         for (k in item.assets) {
-            asset = item.assets[k].name.replace(/"/g, '\"');
+            asset = item.assets[k].name.replace(/"/g, '\\"');
             assets += `${asset};`;
         }
         iocs = "";
         for (k in item.iocs) {
-            ioc = item.iocs[k].name.replace(/"/g, '\"');
+            ioc = item.iocs[k].name.replace(/"/g, '\\"');
             iocs += `${ioc};`;
         }
         csv_data += `"${item.event_date}","${title}","${content_parsed}","${item.event_tz}","${item.event_date_wtz}","${item.category_name}","${tags}","${assets}","${iocs}","${item.user}","${item.event_added}"\n`;
